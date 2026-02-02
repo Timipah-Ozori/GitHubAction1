b806b5ff9fe25702e6cd75deb3ff6c858ec63dcf
Timipah <youremail@example.com>
2026-02-02 13:10:12 +0100

    Add Jest tests and refactor Express app for CI/CD project

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const app = require('./app');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
